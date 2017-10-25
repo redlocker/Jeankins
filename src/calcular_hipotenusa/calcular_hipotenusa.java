67e0a6e3b56d0cb23414fd06b2ec699e64af32c9
Andres Patiño <andypipo345@hotmail.es>
2017-10-24 20:38:14 -0500

    aaaa

---
+++ src/calcular_hipotenusa/calcular_hipotenusa.java
@@ -0,0 +1,10 @@
+
+package calcular_hipotenusa;
+
+public class calcular_hipotenusa {
+    
+    public double hipotenusa(double ladoa, double ladob)
+    {
+        return Math.sqrt(Math.pow(ladoa,2) + Math.pow(ladob,2));
+    }
+}
